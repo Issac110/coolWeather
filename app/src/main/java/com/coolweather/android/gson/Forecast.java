8337ec5680fba2e76bc252250c78639f7e95370b
IssacSu <swfdeyx@qq.com>
2019-04-07 17:53:15 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/coolweather/android/gson/Forecast.java
@@ -0,0 +1,28 @@
+package com.coolweather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by IssacSu on 2019/4/7.
+ */
+
+public class Forecast {
+
+    public String date;
+
+    @SerializedName("tmp")
+    public Temprature temprature;
+
+    @SerializedName("cond")
+    public More more;
+
+    public class Temprature {
+        public String max;
+        public String min;
+    }
+
+    public class More {
+        @SerializedName("txt_d")
+        public String info;
+    }
+}
